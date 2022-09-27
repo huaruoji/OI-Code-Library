ee590dc2c1d325a7aca9030dc273cc829e01475f
huaruoji <huaruoji@gmail.com>
2022-09-28 07:52:26 +0800

    update: 高精度.cpp

--- Z 其他/高精度.cpp
+++ Z 其他/高精度.cpp
@@ -1,67 +1,115 @@
-const int S = 45; 
-struct bigInt {
-	int num[S], len;
-	void out() {
-		if(len == 0) cout << 0;
-		else for(int i = len; i >= 1; i--) cout << num[i];
-	}
-	bigInt() {
-		len = 0;
-		memset(num, 0, sizeof num);
-	}
-	bigInt(int t) {
-		len = 0;
-		memset(num, 0, sizeof num);
-		while(t) {
-			num[++len] = t % 10;
-			t /= 10;
-		}
-	}
+struct BigInteger {
+    typedef unsigned long long LL;
+ 
+    static const int BASE = 100000000;
+    static const int WIDTH = 8;
+    vector<int> s;
+ 
+    BigInteger& clean(){while(!s.back()&&s.size()>1)s.pop_back(); return *this;}
+    BigInteger(LL num = 0) {*this = num;}
+    BigInteger(string s) {*this = s;}
+    BigInteger& operator = (long long num) {
+        s.clear();
+        do {
+            s.push_back(num % BASE);
+            num /= BASE;
+        } while (num > 0);
+        return *this;
+    }
+    BigInteger& operator = (const string& str) {
+        s.clear();
+        int x, len = (str.length() - 1) / WIDTH + 1;
+        for (int i = 0; i < len; i++) {
+            int end = str.length() - i*WIDTH;
+            int start = max(0, end - WIDTH);
+            sscanf(str.substr(start,end-start).c_str(), "%d", &x);
+            s.push_back(x);
+        }
+        return (*this).clean();
+    }
+ 
+    BigInteger operator + (const BigInteger& b) const {
+        BigInteger c; c.s.clear();
+        for (int i = 0, g = 0; ; i++) {
+            if (g == 0 && i >= s.size() && i >= b.s.size()) break;
+            int x = g;
+            if (i < s.size()) x += s[i];
+            if (i < b.s.size()) x += b.s[i];
+            c.s.push_back(x % BASE);
+            g = x / BASE;
+        }
+        return c;
+    }
+    BigInteger operator - (const BigInteger& b) const {
+        assert(b <= *this); // 减数不能大于被减数
+        BigInteger c; c.s.clear();
+        for (int i = 0, g = 0; ; i++) {
+            if (g == 0 && i >= s.size() && i >= b.s.size()) break;
+            int x = s[i] + g;
+            if (i < b.s.size()) x -= b.s[i];
+            if (x < 0) {g = -1; x += BASE;} else g = 0;
+            c.s.push_back(x);
+        }
+        return c.clean();
+    }
+    BigInteger operator * (const BigInteger& b) const {
+        int i, j; LL g;
+        vector<LL> v(s.size()+b.s.size(), 0);
+        BigInteger c; c.s.clear();
+        for(i=0;i<s.size();i++) for(j=0;j<b.s.size();j++) v[i+j]+=LL(s[i])*b.s[j];
+        for (i = 0, g = 0; ; i++) {
+            if (g ==0 && i >= v.size()) break;
+            LL x = v[i] + g;
+            c.s.push_back(x % BASE);
+            g = x / BASE;
+        }
+        return c.clean();
+    }
+    BigInteger operator / (const BigInteger& b) const {
+        assert(b > 0);  // 除数必须大于0
+        BigInteger c = *this;       // 商:主要是让c.s和(*this).s的vector一样大
+        BigInteger m;               // 余数:初始化为0
+        for (int i = s.size()-1; i >= 0; i--) {
+            m = m*BASE + s[i];
+            c.s[i] = bsearch(b, m);
+			m -= b*c.s[i];
+        }
+        return c.clean();
+    }
+    BigInteger operator % (const BigInteger& b) const { //方法与除法相同
+        BigInteger c = *this;
+        BigInteger m;
+        for (int i = s.size()-1; i >= 0; i--) {
+            m = m*BASE + s[i];
+            c.s[i] = bsearch(b, m);
+            m -= b*c.s[i];
+        }
+        return m;
+    }
+    // 二分法找出满足bx<=m的最大的x
+    int bsearch(const BigInteger& b, const BigInteger& m) const{
+        int L = 0, R = BASE-1, x;
+        while (1) {
+            x = (L+R)>>1;
+            if (b*x<=m) {if (b*(x+1)>m) return x; else L = x;}
+            else R = x;
+        }
+    }
+    BigInteger& operator += (const BigInteger& b) {*this = *this + b; return *this;}
+    BigInteger& operator -= (const BigInteger& b) {*this = *this - b; return *this;}
+    BigInteger& operator *= (const BigInteger& b) {*this = *this * b; return *this;}
+    BigInteger& operator /= (const BigInteger& b) {*this = *this / b; return *this;}
+    BigInteger& operator %= (const BigInteger& b) {*this = *this % b; return *this;}
+ 
+    bool operator < (const BigInteger& b) const {
+        if (s.size() != b.s.size()) return s.size() < b.s.size();
+        for (int i = s.size()-1; i >= 0; i--)
+            if (s[i] != b.s[i]) return s[i] < b.s[i];
+        return false;
+    }
+    bool operator >(const BigInteger& b) const{return b < *this;}
+    bool operator<=(const BigInteger& b) const{return !(b < *this);}
+    bool operator>=(const BigInteger& b) const{return !(*this < b);}
+    bool operator!=(const BigInteger& b) const{return b < *this || *this < b;}
+    bool operator==(const BigInteger& b) const{return !(b < *this) && !(b > *this);}
 };
-bigInt operator * (const bigInt &a, const bigInt &b) {
-	bigInt ret;
-	int x = 0;
-	memset(ret.num, 0, sizeof ret.num);
-	for(int i = 1; i <= a.len; i++) {
-		for(int j = 1; j <= b.len; j++) {
-			int cur = i + j - 1;
-			ret.num[cur] += a.num[i] * b.num[j] + x;
-			x = ret.num[cur] / 10;
-			ret.num[cur] %= 10;
-		}
-		ret.num[i + b.len] = x, x = 0;
-	}
-	for(int i = S - 1; i >= 0; i--) {
-		if(ret.num[i] && !ret.len) {
-			ret.len = i;
-			break;
-		}
-	}
-	return ret;
-}
-bigInt operator + (const bigInt &a, const bigInt &b) {
-	bigInt ret;
-	int x = 0;
-	memset(ret.num, 0, sizeof ret.num);
-	int width = max(a.len, b.len);
-	for(int i = 1; i <= width; i++) {
-		ret.num[i] = a.num[i] + b.num[i] + x;
-		x = ret.num[i] / 10;
-		ret.num[i] %= 10;
-	}
-	ret.num[width + 1] = x, x = 0;
-	for(int i = S - 1; i >= 0; i--) {
-		if(ret.num[i] && !ret.len) {
-			ret.len = i;
-			break;
-		}
-	}
-	return ret;
-}
-bool operator < (const bigInt &a, const bigInt &b) {
-	if(a.len == b.len) {
-		int t = a.len;
-		while(a.num[t] == b.num[t]) --t;
-		return a.num[t] < b.num[t];
-	} else return a.len < b.len;
-}
