f2dd0bc3d32a9c08d74e3bd669833447a78616e9
huaruoji <huaruoji@gmail.com>
2022-10-03 15:24:36 +0800

    update: add ostream overload

--- Z 其他/高精度.cpp
+++ Z 其他/高精度.cpp
@@ -1,115 +1,160 @@
 struct BigInteger {
-    typedef unsigned long long LL;
- 
-    static const int BASE = 100000000;
-    static const int WIDTH = 8;
-    vector<int> s;
- 
-    BigInteger& clean(){while(!s.back()&&s.size()>1)s.pop_back(); return *this;}
-    BigInteger(LL num = 0) {*this = num;}
-    BigInteger(string s) {*this = s;}
-    BigInteger& operator = (long long num) {
-        s.clear();
-        do {
-            s.push_back(num % BASE);
-            num /= BASE;
-        } while (num > 0);
-        return *this;
+  typedef unsigned long long LL;
+
+  static const int BASE = 100000000;
+  static const int WIDTH = 8;
+  vector<int> s;
+
+  BigInteger& clean() {
+    while (!s.back() && s.size() > 1) s.pop_back();
+    return *this;
+  }
+  BigInteger(LL num = 0) { *this = num; }
+  BigInteger(string s) { *this = s; }
+  BigInteger& operator=(long long num) {
+    s.clear();
+    do {
+      s.emplace_back(num % BASE);
+      num /= BASE;
+    } while (num > 0);
+    return *this;
+  }
+  BigInteger& operator=(const string& str) {
+    s.clear();
+    int x, len = (str.length() - 1) / WIDTH + 1;
+    for (int i = 0; i < len; i++) {
+      int end = str.length() - i * WIDTH;
+      int start = max(0, end - WIDTH);
+      sscanf(str.substr(start, end - start).c_str(), "%d", &x);
+      s.emplace_back(x);
     }
-    BigInteger& operator = (const string& str) {
-        s.clear();
-        int x, len = (str.length() - 1) / WIDTH + 1;
-        for (int i = 0; i < len; i++) {
-            int end = str.length() - i*WIDTH;
-            int start = max(0, end - WIDTH);
-            sscanf(str.substr(start,end-start).c_str(), "%d", &x);
-            s.push_back(x);
-        }
-        return (*this).clean();
+    return (*this).clean();
+  }
+
+  BigInteger operator+(const BigInteger& b) const {
+    BigInteger c;
+    c.s.clear();
+    for (int i = 0, g = 0;; i++) {
+      if (g == 0 && i >= s.size() && i >= b.s.size()) break;
+      int x = g;
+      if (i < s.size()) x += s[i];
+      if (i < b.s.size()) x += b.s[i];
+      c.s.emplace_back(x % BASE);
+      g = x / BASE;
     }
- 
-    BigInteger operator + (const BigInteger& b) const {
-        BigInteger c; c.s.clear();
-        for (int i = 0, g = 0; ; i++) {
-            if (g == 0 && i >= s.size() && i >= b.s.size()) break;
-            int x = g;
-            if (i < s.size()) x += s[i];
-            if (i < b.s.size()) x += b.s[i];
-            c.s.push_back(x % BASE);
-            g = x / BASE;
-        }
-        return c;
+    return c;
+  }
+  BigInteger operator-(const BigInteger& b) const {
+    assert(b <= *this);  // å‡æ•°ä¸èƒ½å¤§äºŽè¢«å‡æ•°
+    BigInteger c;
+    c.s.clear();
+    for (int i = 0, g = 0;; i++) {
+      if (g == 0 && i >= s.size() && i >= b.s.size()) break;
+      int x = s[i] + g;
+      if (i < b.s.size()) x -= b.s[i];
+      if (x < 0) {
+        g = -1;
+        x += BASE;
+      } else
+        g = 0;
+      c.s.emplace_back(x);
     }
-    BigInteger operator - (const BigInteger& b) const {
-        assert(b <= *this); // ¼õÊý²»ÄÜ´óÓÚ±»¼õÊý
-        BigInteger c; c.s.clear();
-        for (int i = 0, g = 0; ; i++) {
-            if (g == 0 && i >= s.size() && i >= b.s.size()) break;
-            int x = s[i] + g;
-            if (i < b.s.size()) x -= b.s[i];
-            if (x < 0) {g = -1; x += BASE;} else g = 0;
-            c.s.push_back(x);
-        }
-        return c.clean();
+    return c.clean();
+  }
+  BigInteger operator*(const BigInteger& b) const {
+    int i, j;
+    LL g;
+    vector<LL> v(s.size() + b.s.size(), 0);
+    BigInteger c;
+    c.s.clear();
+    for (i = 0; i < s.size(); i++)
+      for (j = 0; j < b.s.size(); j++) v[i + j] += LL(s[i]) * b.s[j];
+    for (i = 0, g = 0;; i++) {
+      if (g == 0 && i >= v.size()) break;
+      LL x = v[i] + g;
+      c.s.emplace_back(x % BASE);
+      g = x / BASE;
     }
-    BigInteger operator * (const BigInteger& b) const {
-        int i, j; LL g;
-        vector<LL> v(s.size()+b.s.size(), 0);
-        BigInteger c; c.s.clear();
-        for(i=0;i<s.size();i++) for(j=0;j<b.s.size();j++) v[i+j]+=LL(s[i])*b.s[j];
-        for (i = 0, g = 0; ; i++) {
-            if (g ==0 && i >= v.size()) break;
-            LL x = v[i] + g;
-            c.s.push_back(x % BASE);
-            g = x / BASE;
-        }
-        return c.clean();
+    return c.clean();
+  }
+  BigInteger operator/(const BigInteger& b) const {
+    assert(b > 0);         // é™¤æ•°å¿…é¡»å¤§äºŽ0
+    BigInteger c = *this;  // å•†:ä¸»è¦æ˜¯è®©c.så’Œ(*this).sçš„vectorä¸€æ ·å¤§
+    BigInteger m;          // ä½™æ•°:åˆå§‹åŒ–ä¸º0
+    for (int i = s.size() - 1; i >= 0; i--) {
+      m = m * BASE + s[i];
+      c.s[i] = bsearch(b, m);
+      m -= b * c.s[i];
     }
-    BigInteger operator / (const BigInteger& b) const {
-        assert(b > 0);  // ³ýÊý±ØÐë´óÓÚ0
-        BigInteger c = *this;       // ÉÌ:Ö÷ÒªÊÇÈÃc.sºÍ(*this).sµÄvectorÒ»Ñù´ó
-        BigInteger m;               // ÓàÊý:³õÊ¼»¯Îª0
-        for (int i = s.size()-1; i >= 0; i--) {
-            m = m*BASE + s[i];
-            c.s[i] = bsearch(b, m);
-			m -= b*c.s[i];
-        }
-        return c.clean();
+    return c.clean();
+  }
+  BigInteger operator%(const BigInteger& b) const {  // æ–¹æ³•ä¸Žé™¤æ³•ç›¸åŒ
+    BigInteger c = *this;
+    BigInteger m;
+    for (int i = s.size() - 1; i >= 0; i--) {
+      m = m * BASE + s[i];
+      c.s[i] = bsearch(b, m);
+      m -= b * c.s[i];
     }
-    BigInteger operator % (const BigInteger& b) const { //·½·¨Óë³ý·¨ÏàÍ¬
-        BigInteger c = *this;
-        BigInteger m;
-        for (int i = s.size()-1; i >= 0; i--) {
-            m = m*BASE + s[i];
-            c.s[i] = bsearch(b, m);
-            m -= b*c.s[i];
-        }
-        return m;
+    return m;
+  }
+  // äºŒåˆ†æ³•æ‰¾å‡ºæ»¡è¶³bx<=mçš„æœ€å¤§çš„x
+  int bsearch(const BigInteger& b, const BigInteger& m) const {
+    int L = 0, R = BASE - 1, x;
+    while (1) {
+      x = (L + R) >> 1;
+      if (b * x <= m) {
+        if (b * (x + 1) > m)
+          return x;
+        else
+          L = x;
+      } else
+        R = x;
     }
-    // ¶þ·Ö·¨ÕÒ³öÂú×ãbx<=mµÄ×î´óµÄx
-    int bsearch(const BigInteger& b, const BigInteger& m) const{
-        int L = 0, R = BASE-1, x;
-        while (1) {
-            x = (L+R)>>1;
-            if (b*x<=m) {if (b*(x+1)>m) return x; else L = x;}
-            else R = x;
-        }
+  }
+  BigInteger& operator+=(const BigInteger& b) {
+    *this = *this + b;
+    return *this;
+  }
+  BigInteger& operator-=(const BigInteger& b) {
+    *this = *this - b;
+    return *this;
+  }
+  BigInteger& operator*=(const BigInteger& b) {
+    *this = *this * b;
+    return *this;
+  }
+  BigInteger& operator/=(const BigInteger& b) {
+    *this = *this / b;
+    return *this;
+  }
+  BigInteger& operator%=(const BigInteger& b) {
+    *this = *this % b;
+    return *this;
+  }
+
+  bool operator<(const BigInteger& b) const {
+    if (s.size() != b.s.size()) return s.size() < b.s.size();
+    for (int i = s.size() - 1; i >= 0; i--)
+      if (s[i] != b.s[i]) return s[i] < b.s[i];
+    return false;
+  }
+  bool operator>(const BigInteger& b) const { return b < *this; }
+  bool operator<=(const BigInteger& b) const { return !(b < *this); }
+  bool operator>=(const BigInteger& b) const { return !(*this < b); }
+  bool operator!=(const BigInteger& b) const { return b < *this || *this < b; }
+  bool operator==(const BigInteger& b) const {
+    return !(b < *this) && !(b > *this);
+  }
+  
+  ostream& operator<<(ostream &os) {
+    int len = s.size();
+    if(len) os << to_string(s[len - 1]);
+    for(int i = len - 2; i >= 0; i--) {
+      string t = to_string(s[i]);
+      for(int i = WIDTH - t.size(); i >= 1; i--) os << 0;
+      os << t;
     }
-    BigInteger& operator += (const BigInteger& b) {*this = *this + b; return *this;}
-    BigInteger& operator -= (const BigInteger& b) {*this = *this - b; return *this;}
-    BigInteger& operator *= (const BigInteger& b) {*this = *this * b; return *this;}
-    BigInteger& operator /= (const BigInteger& b) {*this = *this / b; return *this;}
-    BigInteger& operator %= (const BigInteger& b) {*this = *this % b; return *this;}
- 
-    bool operator < (const BigInteger& b) const {
-        if (s.size() != b.s.size()) return s.size() < b.s.size();
-        for (int i = s.size()-1; i >= 0; i--)
-            if (s[i] != b.s[i]) return s[i] < b.s[i];
-        return false;
-    }
-    bool operator >(const BigInteger& b) const{return b < *this;}
-    bool operator<=(const BigInteger& b) const{return !(b < *this);}
-    bool operator>=(const BigInteger& b) const{return !(*this < b);}
-    bool operator!=(const BigInteger& b) const{return b < *this || *this < b;}
-    bool operator==(const BigInteger& b) const{return !(b < *this) && !(b > *this);}
+  }
 };
+ostream& operator<<(ostream &os, const BigInteger &num) { num.operator<<(os); }
