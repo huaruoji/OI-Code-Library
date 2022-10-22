2a1d60cc1fa8c7a434fa029d3456f6b99cbfd7e3
huaruoji <huaruoji@gmail.com>
2022-10-22 16:46:47 +0800

    update(快速幂): wrong implementation

--- D 数学/D0 数学-快速幂.cpp
+++ D 数学/D0 数学-快速幂.cpp
@@ -1,8 +1,8 @@
 int fpm(int a, int b, int c) {
 	int ans = 1 % c;
 	while(b) {
-		if(b & 1) ans = (ll)ans * a % c, b >>= 1;
-		a = (ll)a * a % c;
+		if(b & 1) ans = (ll)ans * a % c;
+		b >>= 1, a = (ll)a * a % c;
 	}
 	return ans;
 }
