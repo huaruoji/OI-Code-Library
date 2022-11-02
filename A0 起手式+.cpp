3408c0f57ba0f330a701296abcf05d9ae2b31ccb
huaruoji <huaruoji@gmail.com>
2022-11-02 11:02:43 +0800

    update(起手式): 增加贺来的 ModInt 类，FastReader 用重载右移运算符实现

--- A0 起手式+.cpp
+++ A0 起手式+.cpp
@@ -2,31 +2,68 @@
 
 using namespace std;
 void debug_out() { cerr << '\n'; }
-template <typename Head, typename... Tail>
+template<typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) { cerr << ' ' << H, debug_out(T...); }
 #define debug(...) cerr << '[' << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-namespace FI {
-	#define getchar() (S == T && (T = (S = B) + fread(B, 1, 1 << 16, stdin), S == T) ? EOF : *S++)
+struct FastReader {
+  // attention: once you use fin you can not use cin or scanf anymore.
 	char B[1 << 16], *S = B, *T = B;
-	template<typename Tp> inline void read(Tp &o) {
+  inline char getchar() { return S == T && (T = (S = B) + fread(B, 1, 1 << 16, stdin), S == T) ? EOF : *S++; }
+	template<typename Tp> FastReader& operator>>(Tp &o) {
 		o = 0; bool s = 0; char c = getchar();
 		while(c > '9' || c < '0') s |= c == '-', c = getchar();
 		while(c >= '0' && c <= '9') o = o * 10 + c - '0', c = getchar();
-		if(s) o = -o;
+		if(s) { o = -o; } return *this;
 	}
-} using FI::read;
+} fin;
+struct ModInt {
+  static unsigned M;
+  unsigned x;
+  ModInt() : x(0U) {}
+  ModInt(unsigned x_) : x(x_ % M) {}
+  ModInt(unsigned long long x_) : x(x_ % M) {}
+  ModInt(int x_) : x(((x_ %= static_cast<int>(M)) < 0) ? (x_ + static_cast<int>(M)) : x_) {}
+  ModInt(long long x_) : x(((x_ %= static_cast<long long>(M)) < 0) ? (x_ + static_cast<long long>(M)) : x_) {}
+  ModInt &operator+=(const ModInt &a) { x = ((x += a.x) >= M) ? (x - M) : x; return *this; }
+  ModInt &operator-=(const ModInt &a) { x = ((x -= a.x) >= M) ? (x + M) : x; return *this; }
+  ModInt &operator*=(const ModInt &a) { x = (static_cast<unsigned long long>(x) * a.x) % M; return *this; }
+  ModInt &operator/=(const ModInt &a) { return (*this *= a.inv()); }
+  ModInt pow(long long e) const {
+    if(e < 0) return inv().pow(-e);
+    ModInt a = *this, b = 1U; for(; e; e >>= 1) { if (e & 1) b *= a; a *= a; } return b;
+  }
+  ModInt inv() const {
+    unsigned a = M, b = x; int y = 0, z = 1;
+    for(; b; ) { const unsigned q = a / b; const unsigned c = a - q * b; a = b; b = c; const int w = y - static_cast<int>(q) * z; y = z; z = w; }
+    assert(a == 1U); return ModInt(y);
+  }
+  ModInt operator+() const { return *this; }
+  ModInt operator-() const { ModInt a; a.x = x ? (M - x) : 0U; return a; }
+  ModInt operator+(const ModInt &a) const { return (ModInt(*this) += a); }
+  ModInt operator-(const ModInt &a) const { return (ModInt(*this) -= a); }
+  ModInt operator*(const ModInt &a) const { return (ModInt(*this) *= a); }
+  ModInt operator/(const ModInt &a) const { return (ModInt(*this) /= a); }
+  template<class T> friend ModInt operator+(T a, const ModInt &b) { return (ModInt(a) += b); }
+  template<class T> friend ModInt operator-(T a, const ModInt &b) { return (ModInt(a) -= b); }
+  template<class T> friend ModInt operator*(T a, const ModInt &b) { return (ModInt(a) *= b); }
+  template<class T> friend ModInt operator/(T a, const ModInt &b) { return (ModInt(a) /= b); }
+  explicit operator bool() const { return x; }
+  bool operator==(const ModInt &a) const { return (x == a.x); }
+  bool operator!=(const ModInt &a) const { return (x != a.x); }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) { return os << a.x; }
+};
+unsigned ModInt::M;
 #define fi first
 #define se second
 #define mp make_pair
 #define eb emplace_back
 #define all(x) x.begin(), x.end()
-typedef long long ll;
-typedef pair<int, int> pii;
+using ll = long long;
+using pii = pair<int, int>;
 template<typename T> inline void cmin(T &a, T b) { if(a > b) a = b; }
 template<typename T> inline void cmax(T &a, T b) { if(a < b) a = b; }
 
 int main() {
-	cout << "123";
   
-	return 0;
+  return 0;
 }
